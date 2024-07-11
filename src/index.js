ebdc54902d0138febf3820ceb52a3617f201b4b1
PhoenixHO <oussamahaddouche0@gmail.com>
2024-07-11 01:30:55 +0100

    add front-end and basic facial recognition

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+try {
+	require('electron-reloader')(module);
+} catch { }
+
+let win;
+
+function createWindow() {
+	win = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		frame: false,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true
+		}
+	});
+
+	win.loadURL('http://localhost:5500/src/index.html');
+	win.on('closed', () => {
+		win.destroy();
+	});
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+});
+
+ipcMain.on('minimize-window', () => {
+	win.minimize();
+});
+ipcMain.on('maximize-window', () => {
+	win.isMaximized() ? win.unmaximize() : win.maximize();
+});
+ipcMain.on('close-window', () => {
+	win.close();
+});
