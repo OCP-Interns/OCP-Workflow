5af72e72eafd7db1615385c3f21de487548fa324
marouane mrf <marouanemorfi@gmail.com>
2024-07-11 08:54:22 +0100

    msg

---
+++ src/api/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import face_recognition
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+known_face_encodings = []
+known_face_names = ["Mark Iplier"]
+
+image_of_known_person = face_recognition.load_image_file("../../assets/i_mark.png")
+known_person_face_encoding = face_recognition.face_encodings(image_of_known_person)[0]
+
+known_face_encodings.append(known_person_face_encoding)
+
+@app.route('/face-recognition', methods=['POST'])
+def face_recognition_api():
+	if 'file' not in request.files:
+		return jsonify({'error': 'No file part'})
+	
+	file = request.files['file']
+
+	if file.filename == '':
+		return jsonify({'error': 'No selected file'})
+	
+	if file:
+		image = face_recognition.load_image_file(file)
+		face_locations = face_recognition.face_locations(image)
+		face_encodings = face_recognition.face_encodings(image, face_locations)
+
+		face_names = []
+		for face_encoding in face_encodings:
+			matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+			name = "Unknown Person"
+
+			face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+			best_match_index = np.argmin(face_distances)
+			if matches[best_match_index]:
+				name = known_face_names[best_match_index]
+
+			face_names.append(name)
+
+		print('Found:', face_names)
+
+		return jsonify({'face_names': face_names})
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ src/script.js
@@ -0,0 +1,59 @@
+document.getElementById('face-id-btn').addEventListener('click', async () => {
+	//const imageBlob = await fetch('../assets/e.png').then((response) => {
+	//	return response.blob();
+	//});
+
+	//const formData = new FormData();
+	//formData.append('file', imageBlob, 'e.png');
+
+	//const response = await axios({
+	//	method: 'post',
+	//	url: 'http://localhost:5000/face-recognition',
+	//	data: formData,
+	//	headers: {
+	//		'Content-Type': 'multipart/form-data',
+	//		'Access-Control-Allow-Origin': '*'
+	//	}
+	//}).then((response) => {
+	//	console.log(response.data);
+	//}).catch((error) => {
+	//	console.error('Error: ', error);
+	//});
+
+	try {
+		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+		const track = stream.getVideoTracks()[0];
+		const imageCapture = new ImageCapture(track);
+
+		const blob = await imageCapture.takePhoto();
+
+		track.stop();
+
+		const formData = new FormData();
+		formData.append('file', blob, 'capture.png');
+
+		const response = await axios({
+			method: 'post',
+			url: 'http://localhost:5000/face-recognition',
+			data: formData,
+			headers: {
+				'Content-Type': 'multipart/form-data',
+				'Access-Control-Allow-Origin': '*'
+			}
+		});
+
+		console.log(response.data);
+	} catch (error) {
+		console.error('Error: ', error);
+	}
+});
+
+document.getElementById('minimize-btn').addEventListener('click', () => {
+	window.electron.minimize();
+});
+document.getElementById('maximize-btn').addEventListener('click', () => {
+	window.electron.maximize();
+});
+document.getElementById('close-btn').addEventListener('click', () => {
+	window.electron.close();
+});
